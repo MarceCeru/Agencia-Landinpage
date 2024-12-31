68438640854e7bdedc30bd14eea2e7746df8a6f6
Cerutti Marcelo <134024715+MarceCeru@users.noreply.github.com>
2024-12-31 17:31:52 -0300

    Update1 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://marceceru.github.io/Agency-Landin-page/',
+  base: 'https://marceceru.github.io/Agency-Landin-page/'
 })
