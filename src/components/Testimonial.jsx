6e06cba0ca02e57f586e4f764894fe805007cb09
Cerutti Marcelo <134024715+MarceCeru@users.noreply.github.com>
2025-01-03 17:07:36 -0300

    Ajustes

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,7 +2,7 @@
 export const Testimonial = ({img, Testimonial, name, position}) => {
   return (
     <div className="flex flex-col items-center text-center mb-[64px] lg:mb-[164px]">
-        <img className="w-[72px] rounded-full" src={img} alt="photo" />
+        <img className="w-[71.9px] rounded-full" src={img} alt="photo" />
         <p className="my-8 font-Barlow text-Very-dark-grayish-blue px-[28px] max-w-[440px] text-[18px] lg:my-16">{Testimonial}</p>
         <h3 className="font-Fraunces text-[18px] text-Very-dark-desatured-blue ">{name}</h3>
         <p className="text-Grayish-blue font-Barlow text-[14px]">{position}</p>
